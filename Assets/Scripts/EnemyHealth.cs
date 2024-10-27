4c35eb1cc1a3c015bfe2698aae6cb2b0408fa8a5
vhhexcalib <vhhexcalib@gmail.com>
2024-10-27 23:12:23 +0700

    apply ui, stuff into level2,  fix dmg wizard tower

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,17 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     [Header("Attributes")]
-    [SerializeField] private int hitPoints = 2;
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+    public delegate void EnemyDestroyed();
+    public event EnemyDestroyed OnDestroyed;
 
-    public void TakeDamage(int dmg)
+    private void Start()
     {
-        hitPoints -= dmg;
-        if(hitPoints <= 0)
+        currentHealth = maxHealth;
+    }
+
+    public void SetHealth(float newHealth)
+    {
+        maxHealth = newHealth;
+        currentHealth = newHealth;
+        Debug.Log("Enemy health set to: " + newHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Enemy took damage: " + damage + ", Current Health: " + currentHealth);
+        if (currentHealth <= 0f)
         {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy died.");
+        OnDestroyed?.Invoke();
+        EnemySpawners.onEnemyDestroy.Invoke();
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Base"))
+        {
+            Debug.Log("Enemy reached the base and died.");
+            OnDestroyed?.Invoke();
             EnemySpawners.onEnemyDestroy.Invoke();
             Destroy(gameObject);
         }
--- Assets/Scripts/EnemyScriptLevel2/EnemySpawners.cs
+++ Assets/Scripts/EnemyScriptLevel2/EnemySpawners.cs
@@ -1,42 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
-
 
 public class EnemySpawners : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private Transform[] pathPoints;
 
     [Header("Attributes")]
     [SerializeField] private int baseEnemies = 8;
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficultyScalingFactor = 0.75f;
-
+    [SerializeField] private float spawnDelay = 5f;
+    [SerializeField] private float baseEnemyHealth = 100f;
+    [SerializeField] private float healthIncrementPerWave = 5f; 
     [Header("Events")]
     public static UnityEvent onEnemyDestroy = new UnityEvent();
 
     private int currentWave = 1;
+    private int maxWaves = 5;
     private float timeSinceLastSpawn;
     private int enemiesAlives;
     private int enemiesLeftToSpawn;
     private bool isSpawning = false;
+
     private void Awake()
     {
-        onEnemyDestroy.AddListener(EnemyDestroyed);
-
+        onEnemyDestroy.AddListener(OnEnemyDestroyed);
     }
+
     private void Start()
     {
-        StartCoroutine(StartWave());
+        StartCoroutine(SpawnWave());
     }
+
     private void Update()
     {
         if (!isSpawning) return;
+
         timeSinceLastSpawn += Time.deltaTime;
         if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
         {
@@ -46,36 +50,98 @@ public class EnemySpawners : MonoBehaviour
             timeSinceLastSpawn = 0f;
         }
 
-        if(enemiesAlives == 0 && enemiesLeftToSpawn == 0)
+        if (enemiesAlives == 0 && enemiesLeftToSpawn == 0 && isSpawning)
         {
             EndWave();
         }
     }
-    private void EnemyDestroyed()
+
+    private IEnumerator SpawnWave()
     {
-        enemiesAlives--;
+        while (currentWave <= maxWaves)
+        {
+            DisplayMessage($"Starting wave {currentWave} with {EnemiesPerWave()} enemies.");
+
+            enemiesLeftToSpawn = EnemiesPerWave();
+            enemiesAlives = 0;
+            isSpawning = true;
+
+            for (int i = 0; i < enemiesLeftToSpawn; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(spawnDelay);
+            }
+
+            DisplayMessage($"All enemies for wave {currentWave} spawned.");
+
+            while (enemiesAlives > 0)
+            {
+                yield return null;
+            }
+
+            DisplayMessage($"Wave {currentWave} complete. Moving to next wave...");
+            GameManager.instance.WaveCompleted();
+
+            currentWave++;
+
+            if (currentWave > maxWaves)
+            {
+                DisplayMessage("Max number of waves reached.");
+                GameManager.instance.SetAllWavesCompleted();
+                yield break;
+            }
+
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
     }
 
-    private IEnumerator StartWave()
-    {
-        yield return new WaitForSeconds(timeBetweenWaves);
-        isSpawning = true;
-        enemiesLeftToSpawn = EnemiesPerWave();
-    }
     private void EndWave()
     {
         isSpawning = false;
-        currentWave++;
         timeSinceLastSpawn = 0f;
-        StartCoroutine(StartWave());
+        StartCoroutine(SpawnWave());
     }
+
     private void SpawnEnemy()
     {
         GameObject prefabToSpawn = enemyPrefabs[0];
-        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+        GameObject enemy = Instantiate(prefabToSpawn, spawnPoint.position, Quaternion.identity);
+        enemy.SetActive(true);
+
+        GameManager.instance.RegisterEnemy();
+
+        CreepMovement creepMovement = enemy.GetComponent<CreepMovement>();
+        if (creepMovement != null && pathPoints.Length > 0)
+        {
+            creepMovement.pathPoints = pathPoints;
+        }
+
+        EnemyHealth creepHealth = enemy.GetComponent<EnemyHealth>();
+        if (creepHealth != null)
+        {
+            creepHealth.OnDestroyed += OnEnemyDestroyed;
+            float newHealth = baseEnemyHealth + (healthIncrementPerWave * (currentWave - 1));
+            creepHealth.SetHealth(newHealth);
+            DisplayMessage($"Enemy spawned with {newHealth} health. Remaining enemies to spawn: {enemiesLeftToSpawn}");
+        }
     }
+
+    private void OnEnemyDestroyed()
+    {
+        enemiesAlives--;
+
+        GameManager.instance.EnemyKilled();
+
+        DisplayMessage($"Enemy destroyed. Remaining enemies alive: {enemiesAlives}");
+    }
+
     private int EnemiesPerWave()
     {
         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+
+    private void DisplayMessage(string message)
+    {
+        Debug.Log(message);
     }
 }
