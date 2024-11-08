f16419a8b15eca02e168350298f93a86c03a56e9
vhhexcalib <vhhexcalib@gmail.com>
2024-11-08 11:37:13 +0700

    fix

--- Assets/Scripts/TowerControl.cs
+++ Assets/Scripts/TowerControl.cs
@@ -1,19 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerControl : MonoBehaviour
 {
     public GameObject wizardTowerPrefab;
-    public Transform spawnPosition; 
+    public Transform spawnPosition;
+    public Button createTowerButton; 
+
+    private bool canCreateTower = true;
+
     public void CreateTower()
     {
-        GameObject newTower = Instantiate(wizardTowerPrefab, spawnPosition.position, Quaternion.identity);
-        StartCoroutine(DestroyTowerAfterTime(newTower, 20f));
+        if (canCreateTower)
+        {
+            GameObject newTower = Instantiate(wizardTowerPrefab, spawnPosition.position, Quaternion.identity);
+            StartCoroutine(DestroyTowerAfterTime(newTower, 20f));
+            canCreateTower = false;
+            if (createTowerButton != null)
+            {
+                createTowerButton.interactable = false;
+            }
+        }
     }
+
     private IEnumerator DestroyTowerAfterTime(GameObject tower, float delay)
     {
         yield return new WaitForSeconds(delay);
         Destroy(tower);
+    }
+
+    public void ResetTowerCreation()
+    {
+        canCreateTower = true;
+        if (createTowerButton != null)
+        {
+            createTowerButton.interactable = true; 
+        }
     }
 }
